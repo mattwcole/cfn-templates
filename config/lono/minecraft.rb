68cd8ae11f88fd1f0781ce4db23f8d2bbe8a5a4e
Matt Cole <matt-cole@hotmail.co.uk>
2017-01-01 07:28:00 +0000

    Update Minecraft to 1.9

--- config/lono/minecraft.rb
+++ config/lono/minecraft.rb
@@ -9,9 +9,9 @@ chef_json = {
     'mount_device' => '/dev/xvdf',
     'memory' => '2048',
     'server_name' => 'minecloud',
-    'jar_url' => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.8.9/minecraft_server.1.8.9.jar',
-    'jar_checksum' => 'c18e4245073aaff580eb7359902f0251436568b1647a9e443a924cdb73fa8312',
-    'version' => '1.8.9',
+    'jar_url' => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.9/minecraft_server.1.9.jar',
+    'jar_checksum' => '38a797f50c71f55202e2135a30302cf3a5c8cb494c6d225b88599542957d3a7d',
+    'version' => '1.9',
     'properties' => {
       'difficulty' => '3',
       'white-list' => 'true'
@@ -42,7 +42,7 @@ chef_json = {
 }
 
 template 'minecraft.json' do
-  source 'instance_store_instance_with_data_volume.json.erb'
+  source 'ebs_instance_with_data_volume.json.erb'
   variables(
     init_script: 'ubuntu_init.sh',
     stack_description: 'Minecraft Server',
@@ -52,7 +52,7 @@ template 'minecraft.json' do
     default_key_name: 'minecraft',
     dns_record: 'minecraft.matt-cole.co.uk.',
     instance_start_timeout: 'PT10M',
-    ami_map: AwsConstants::UBUNTU_14_04_INSTANCE_STORE_MAP,
+    ami_map: AwsConstants::UBUNTU_14_04_EBS_SSD_MAP,
     chef_json: chef_json,
     chef_server_url: ENV['CHEF_SERVER_URL'],
     chef_validation_client_name: ENV['CHEF_VALIDATION_CLIENT_NAME'],
