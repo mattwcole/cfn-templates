c626d6734c63932417b78cd38516667d5b552bc4
Matt Cole <matt@matt-cole.co.uk>
2022-05-01 17:31:32 +0100

    Pin pystache version in ubuntu init

--- templates/user_data/ubuntu_init.sh
+++ templates/user_data/ubuntu_init.sh
@@ -2,4 +2,5 @@
 
 apt-get update
 apt-get -y install python3-pip
+pip3 install pystache==0.5.4  # https://www.reddit.com/r/aws/comments/rcpeft/awscfnbootstrap_pip_install_broken_by_new/
 pip3 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-2.0-6.tar.gz
