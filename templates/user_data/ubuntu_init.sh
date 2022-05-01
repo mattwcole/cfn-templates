669e6169960e9c49e3945cdc577aeff23bffc03c
Matt Cole <matt@matt-cole.co.uk>
2022-05-01 20:25:39 +0100

    Update chef install script

--- templates/user_data/ubuntu_init.sh
+++ templates/user_data/ubuntu_init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 apt-get update
-apt-get -y install python3-pip
+apt-get install -y python3-pip curl
 pip3 install pystache==0.5.4  # https://www.reddit.com/r/aws/comments/rcpeft/awscfnbootstrap_pip_install_broken_by_new/
 pip3 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-2.0-6.tar.gz
