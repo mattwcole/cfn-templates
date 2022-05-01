d162f7f0b570ff274f0b844ddb6df059b737b5b0
Matt Cole <matt@matt-cole.co.uk>
2022-05-01 17:06:38 +0100

    Fix version of aws-cfn-bootstrap-py3

--- templates/user_data/ubuntu_init.sh
+++ templates/user_data/ubuntu_init.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get -y install python3-pip
-pip3 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
+pip3 install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-2.0-6.tar.gz
